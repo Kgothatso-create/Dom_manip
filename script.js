a246e3c45a5ec652930cdcac8485b4c4318ab346
Kgothatso-create <kgothatso@map2tech.com>
2024-04-08 06:14:47 +0200

    did refactoring to the code

--- script.js
+++ script.js
@@ -33,33 +33,55 @@
 let score = 20;
 let highscore = 0;
 let number = Math.trunc(Math.random() * 20) + 1;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent =
-      '⛔ You didnt enter a number';
-  } else if (guess > number) {
+    // document.querySelector('.message').textContent =
+    //   '⛔ You didnt enter a number';
+    displayMessage('⛔ You didnt enter a number');
+  } else if (guess !== number) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '⏫Too High';
+      //   document.querySelector('.message').textContent =
+      //     guess > number ? '⏫Too High' : '⏬Too Low';
+      //   document.querySelector('.message').textContent = '⏫Too High';
+      displayMessage(guess > number ? '⏫Too High' : '⏬Too Low');
       score--;
       document.querySelector('.score').textContent = score;
     } else if (score === 0) {
-      document.querySelector('.message').textContent = 'You Lose';
+      //   document.querySelector('.message').textContent = 'You Lose';
+      displayMessage('You Lose');
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '⏬Too Low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else if (score === 0) {
-      document.querySelector('.message').textContent = 'You Lose';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess === number) {
-    document.querySelector('.message').textContent = '🏆You Win';
+  }
+  //   } else if (guess > number) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '⏫Too High';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else if (score === 0) {
+  //       document.querySelector('.message').textContent = 'You Lose';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   } else if (guess < number) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '⏬Too Low';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else if (score === 0) {
+  //       document.querySelector('.message').textContent = 'You Lose';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
+  else if (guess === number) {
+    // document.querySelector('.message').textContent = '🏆You Win';
+    displayMessage('🏆You Win');
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
