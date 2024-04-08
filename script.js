2351966d9f3b2313ea7f757360e9f93656ed5062
Kgothatso-create <kgothatso@map2tech.com>
2024-04-08 05:44:05 +0200

    added high score

--- script.js
+++ script.js
@@ -31,6 +31,7 @@
 // });
 
 let score = 20;
+let highscore = 0;
 let number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -63,6 +64,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = number;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
 });
 
